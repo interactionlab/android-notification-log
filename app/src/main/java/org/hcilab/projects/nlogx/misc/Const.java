10a5fd96e222c1f4aa8f31ae895c2adc6d459e79
Dominik Weber <weberdo@users.noreply.github.com>
2019-03-21 17:33:41 +0100

    Add version code constant

--- app/src/main/java/org/hcilab/projects/nlogx/misc/Const.java
+++ app/src/main/java/org/hcilab/projects/nlogx/misc/Const.java
@@ -5,6 +5,7 @@ import org.hcilab.projects.nlogx.BuildConfig;
 public class Const {
 
 	public static final boolean DEBUG = BuildConfig.DEBUG;
+	public static final long VERSION  = BuildConfig.VERSION_CODE;
 
 	// Feature flags
 	public static final boolean ENABLE_ACTIVITY_RECOGNITION = true;
